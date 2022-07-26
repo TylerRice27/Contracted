6abb57d1626b9ad4e15651e8ad2edd9ddf2dc61c
TylerRice27 <Tylerrayrice2@gmail.com>
2022-07-26 15:56:47 -0600

    Companies Get function

--- Contracted/Controllers/CompaniesController.cs
+++ Contracted/Controllers/CompaniesController.cs
@@ -1,12 +1,41 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Contracted.Models;
+using Contracted.Services;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Contracted.Controllers
 {
-    public class CompaniesController
+
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CompaniesController : ControllerBase
     {
-        
+
+        private readonly CompaniesService _cs;
+
+        public CompaniesController(CompaniesService cs)
+        {
+            _cs = cs;
+        }
+
+
+        [HttpGet]
+        public ActionResult<List<Company>> Get()
+        {
+
+            try
+            {
+                List<Company> companies = _cs.Get();
+                return Ok(companies);
+            }
+            catch (Exception e)
+            {
+
+                return BadRequest(e.Message);
+            }
+
+
+        }
     }
 }
